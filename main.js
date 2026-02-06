64a6bb101994219af75a4fc4c0b6b93c0e77e110
Xirakkal <pbuch57@outlook.fr>
2026-02-06 11:34:45 +0100

    tweaked css and added content

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
 import './src/css/app.scss';
+
+//la navbar apparaît après le hero
+const nav = document.getElementById('mainNav');
+const hero = document.getElementById('hero');
+
+const observer = new IntersectionObserver(
+  ([entry]) => {
+    if (!entry.isIntersecting) {
+      nav.classList.add('show');
+    } else {
+      nav.classList.remove('show');
+    }
+  },
+  {
+    threshold: 0
+  }
+);
+
+observer.observe(hero);
+
+//ouvrir le portfolio si on clique sur le CTA
+
+const heroCta = document.getElementById('cta-hero');
+const portfolioCollapse = document.getElementById('collapsePortfolio');
+
+heroCta.addEventListener("click", function () { portfolioCollapse.classList.add("show") });
