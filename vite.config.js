dbfc58bbbe7ded72ee66efbfc481f8e17f3994c2
Xirakkal <pbuch57@outlook.fr>
2026-02-11 17:32:52 +0100

    preparation for multi page building

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,30 @@
-import { defineConfig } from 'vite';
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
 
-export default defineConfig({
-  base: './',
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: 'modern-compiler',
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+export default defineConfig(({ command }) => {
+  const isBuild = command === 'build'
+
+  return {
+    base: isBuild ? '/portfolio/' : '/',
+
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'modern-compiler',
+        },
       },
     },
-  },
-});
+
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html'),
+          musicshop: resolve(__dirname, 'musicshop.html'),
+        },
+      },
+    },
+  }
+})
